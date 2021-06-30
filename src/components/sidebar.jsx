87d27b7901fc9fcadb206ff8cd64a30474795516
RK Anik <rkanik.me@gmail.com>
2021-06-30 20:33:24 +0600

    Design break issue solved

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,48 +1,51 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useHistory } from 'react-router-dom'
 
-export default class Sidebar extends Component {
-  render() {
-    return (
+const Sidebar = () => {
+  const history = useHistory()
+  return (
+    <div>
       <div>
-        <div>
-          <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
-          <aside id="colorlib-aside" className="border js-fullheight">
-            <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
-              <h1 id="colorlib-logo"><a href="index.html">Shubham Kumar</a></h1>
-              <span className="email"><i className="icon-mail"></i> shukumarjb@gmail.com</span>
-            </div>
-            <nav id="colorlib-main-menu" role="navigation" className="navbar">
-              <div id="navbar" className="collapse">
-                <ul>
-                  <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
-                  <li><a href="#about" data-nav-section="about">About</a></li>
-                  <li><a href="#projects" data-nav-section="projects">Projects</a></li>
-                  <li><a href="#blog" data-nav-section="blog">Blog</a></li>
-                  <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
-                </ul>
-              </div>
-            </nav>
-            <nav id="colorlib-main-menu">
+        <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-controls="navbar"><i /></nav>
+        <aside id="colorlib-aside" className="border js-fullheight">
+          <div className="text-center">
+            <div className="author-img" style={{ backgroundImage: 'url(images/about.jpg)' }} />
+            <h1 id="colorlib-logo" style={{ cursor: 'pointer' }} onClick={() => history && history.push('/')}>Shubham Kumar</h1>
+            <span className="email"><i className="icon-mail"></i> shukumarjb@gmail.com</span>
+          </div>
+          <nav id="colorlib-main-menu" role="navigation" className="navbar">
+            <div id="navbar" className="collapse">
               <ul>
-                
-                <li><a href="https://www.linkedin.com/in/shubham-kumar-10640b6a/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
-                <li><a href="https://github.com/shubhamkr1" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
-                
+                <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
+                <li><a href="#about" data-nav-section="about">About</a></li>
+                <li><a href="#projects" data-nav-section="projects">Projects</a></li>
+                <li><a href="#blog" data-nav-section="blog">Blog</a></li>
+                <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
               </ul>
-            </nav>
-            <div className="colorlib-footer">
-              <p><small>
-                  Made with <i className="icon-heart" aria-hidden="true" /> and <i className="icon-beer" aria-hidden="true"></i><br></br>
-                  Thanks <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> for inspiration 
-              </small></p>
-              <p><small>
-                Something coming soon !!
-              </small></p>
             </div>
-          </aside>
-        </div>
+          </nav>
+          <nav id="colorlib-main-menu">
+            <ul>
+
+              <li><a href="https://www.linkedin.com/in/shubham-kumar-10640b6a/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
+              <li><a href="https://github.com/shubhamkr1" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+
+            </ul>
+          </nav>
+          <div className="colorlib-footer">
+            <p><small>
+              Made with <i className="icon-heart" aria-hidden="true" /> and <i className="icon-beer" aria-hidden="true"></i><br></br>
+              Thanks <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> for inspiration
+            </small></p>
+            <p><small>
+              Something coming soon !!
+            </small></p>
+          </div>
+        </aside>
       </div>
-    )
-  }
+    </div>
+  )
+
 }
+
+export default Sidebar
